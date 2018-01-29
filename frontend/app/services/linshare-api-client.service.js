7e67e533784236ecefbb30cb7673af6e6b81b449
Sang NGO DOAN <sngodoan@linagora.com>
2018-01-29 10:27:40 +0700

    #2 esnLinshareApiClient: add js doc

--- frontend/app/services/linshare-api-client.service.js
+++ frontend/app/services/linshare-api-client.service.js
@@ -14,24 +14,43 @@
       shareDocuments: shareDocuments
     };
 
+    /**
+     * List user workgroups
+     * @return {Promise} Resolve workgroup list on success
+     */
     function listWorkgroups() {
       return getClient().then(function(client) {
         return client.user.workgroup.list();
       });
     }
 
+    /**
+     * List nodes of a workgroup
+     * @param  {String} workGroupUuid - The workgroup UUID
+     * @param  {Object} options       - (optional) { parent: 'parentNodeUuid', type: 'FOLDER | DOCUMENT' }
+     * @return {Promise}              - Resolve node list on success
+     */
     function listNodes(workGroupUuid, options) {
       return getClient().then(function(client) {
         return client.user.workgroup.listNodes(workGroupUuid, options);
       });
     }
 
+    /**
+     * List documents in user's My space
+     * @return {Promise} - Resolve document list on success
+     */
     function listDocuments() {
       return getClient().then(function(client) {
         return client.user.documents.list();
       });
     }
 
+    /**
+     * Share documents in My space to email addresses
+     * @param  {Object} options - Check for example in https://github.com/linagora/linshare-api-client#share
+     * @return {Promise}        - Resolve share result on success
+     */
     function shareDocuments(options) {
       return getClient().then(function(client) {
         return client.user.shares.shareDocuments(options);
