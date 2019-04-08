11afb93520299539b35eb816c67d6b28bb31643d
Fabien MOYON <fmoyon@linagora.com>
2019-04-08 18:45:52 +0200

    #2707 Disabling linshare module from configuration

--- backend/webserver/api/index.js
+++ backend/webserver/api/index.js
@@ -5,8 +5,9 @@ const express = require('express');
 module.exports = function(dependencies, lib) {
 
   const router = express.Router();
+  const moduleName = 'linagora.esn.linshare';
 
-  require('./token')(dependencies, lib, router);
+  require('./token')(dependencies, lib, router, moduleName);
 
   return router;
 };
