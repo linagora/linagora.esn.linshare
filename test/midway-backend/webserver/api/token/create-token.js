2124f71fa67364c495302cad2b41c1d5ff5f0a91
Christophe HAMERLING <chamerling@linagora.com>
2019-04-16 16:24:11 +0200

    f

--- test/midway-backend/webserver/api/token/create-token.js
+++ test/midway-backend/webserver/api/token/create-token.js
@@ -1,39 +1,27 @@
 /* eslint-disable no-console */
 
-'use strict';
-
 const request = require('supertest');
 const expect = require('chai').expect;
 const path = require('path');
-const MODULE_NAME = 'linagora.esn.linshare';
 
 describe('The create token API: POST /token', () => {
   let app, deployOptions, regularUser;
   const password = 'secret';
 
   beforeEach(function(done) {
-    this.helpers.modules.initMidway(MODULE_NAME, err => {
-      console.log('°°°', self.helpers.modules.current.deps);
-      console.log('°°°', JSON.stringify(self.helpers.modules.current.deps));
-      expect(err).to.not.exist;
-      const application = require(this.testEnv.backendPath + '/webserver/application')(this.helpers.modules.current.deps);
-      const api = require(this.testEnv.backendPath + '/webserver/api')(this.helpers.modules.current.deps, this.helpers.modules.current.lib.lib);
+    const self = this;
 
-      application.use(require('body-parser').json());
-      application.use('/api', api);
+    deployOptions = {
+      fixtures: path.normalize(`${__dirname}/../../../fixtures/deployments`)
+    };
+    app = self.helpers.modules.current.app;
 
-      app = this.helpers.modules.getWebServer(application);
-      deployOptions = {
-        fixtures: path.normalize(`${__dirname}/../../../fixtures/deployments`)
-      };
-
-      this.helpers.api.applyDomainDeployment('general', deployOptions, (err, models) => {
-        if (err) {
-          return done(err);
-        }
-        regularUser = models.users[1];
-        done();
-      });
+    self.helpers.api.applyDomainDeployment('general', deployOptions, (err, models) => {
+      if (err) {
+        return done(err);
+      }
+      regularUser = models.users[1];
+      done();
     });
   });
 
